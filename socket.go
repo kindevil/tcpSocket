2f93d62ed4b2cf3d5c71e33628e5415cc8884db5
kindevil <kindevil@outlook.com>
2019-04-24 21:54:21 +0800

    update

--- socket.go
+++ socket.go
@@ -33,7 +33,7 @@ func (s *Socket) Handle(tcpServer *TcpServer, session *Session) {
 	buffer := make([]byte, 0)
 	readBuffer := make([]byte, 1024)
 	data := make([]byte, 20)
-	packetTypeByte := make([]byte, 4)
+	packetType := int16(0)
 
 	for {
 		l, err := session.Conn.Read(readBuffer)
@@ -43,7 +43,7 @@ func (s *Socket) Handle(tcpServer *TcpServer, session *Session) {
 		}
 
 		buffer = append(buffer, readBuffer[:l]...)
-		buffer, data, packetTypeByte, err = s.Depack(buffer)
+		buffer, data, packetType, err = s.Unpack(buffer)
 		if err != nil {
 			logger.Error(err)
 			return
@@ -53,7 +53,6 @@ func (s *Socket) Handle(tcpServer *TcpServer, session *Session) {
 			continue
 		}
 
-		packetType := s.BytesToInt16(packetTypeByte)
 		if packetType != HEARTBEATPACKET {
 			if tcpServer.Event.OnMessage(tcpServer, session, packetType, data) == false {
 				return
@@ -64,31 +63,51 @@ func (s *Socket) Handle(tcpServer *TcpServer, session *Session) {
 	}
 }
 
-func (s *Socket) Pack(message []byte) []byte {
-	header := s.Int16ToBytes(HEADER)
-	packetType := s.Int16ToBytes(REQUESTPACKET)
-	length := s.Int32ToBytes(len(message))
-	return append(append(append(header, packetType...), length...), message...)
+func (s *Socket) Pack(message []byte) ([]byte, error) {
+	socketPacket := &SocketPacket{
+		Header:     HEADER,
+		PacketType: REQUESTPACKET,
+		Length:     int32(len(message)),
+		Content:    message,
+	}
+
+	buffer := new(bytes.Buffer)
+	var err error
+
+	err = binary.Write(buffer, binary.BigEndian, &socketPacket.Header)
+
+	logger.Info(string(buffer.Bytes()))
+
+	err = binary.Write(buffer, binary.BigEndian, &socketPacket.PacketType)
+	err = binary.Write(buffer, binary.BigEndian, &socketPacket.Length)
+	err = binary.Write(buffer, binary.BigEndian, &socketPacket.Content)
+
+	return buffer.Bytes(), err
 }
 
-func (s *Socket) Depack(buffer []byte) ([]byte, []byte, []byte, error) {
+func (s *Socket) Unpack(buffer []byte) ([]byte, []byte, int16, error) {
 	length := len(buffer)
 
 	if length < 8 {
-		return buffer, nil, nil, nil
+		return buffer, nil, 0, nil
 	}
 
-	if s.BytesToInt16(buffer[:2]) != HEADER {
-		return []byte{}, nil, nil, errors.New("header is illegal")
+	header := int16(0)
+	binary.Read(bytes.NewReader(buffer[0:2]), binary.BigEndian, &header)
+	if header != HEADER {
+		return []byte{}, nil, 0, errors.New("header is illegal")
 	}
 
-	messageLength := s.BytesToInt(buffer[4:8])
-	if length < 8+messageLength {
-		return buffer, nil, nil, nil
+	messageLength := int32(0)
+	binary.Read(bytes.NewReader(buffer[4:8]), binary.BigEndian, &messageLength)
+	if length < 8+int(messageLength) {
+		return buffer, nil, 0, nil
 	}
+
+	packetType := int16(0)
+	binary.Read(bytes.NewReader(buffer[2:4]), binary.BigEndian, &packetType)
 
 	data := buffer[8 : messageLength+8]
-	packetType := buffer[2:4]
 	tbuffer := buffer[messageLength+8:]
 	return tbuffer, data, packetType, nil
 }
